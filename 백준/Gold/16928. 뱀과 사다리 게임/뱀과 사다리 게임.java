537e210a0a43ed6b8142f5a3ab671e98b4745c26
chanbyoung <122460524+chanbyoung@users.noreply.github.com>
2023-05-01 15:40:21 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int[] map = new int [101];
+    static boolean[] visit = new boolean[101];
+    static int[] cnt = new int[101];
+    public static void bfs(){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visit[1] = true;
+        while (!queue.isEmpty()){
+            int x = queue.poll();
+            if(x == 100){
+                System.out.println(cnt[x]);
+                break;
+            }
+            for(int i =1; i<=6; i++) {
+                int X = x + i;
+                if (X > 100 || visit[X]) continue;
+                if (map[X] != 0) {
+                    if(!visit[map[X]]) {
+                        visit[map[X]] = true;
+                        queue.add(map[X]);
+                        cnt[map[X]] = cnt[x] + 1;
+                    }
+                } else {
+                    visit[X] = true;
+                    queue.add(X);
+                    cnt[X] = cnt[x] + 1;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            map[x]=y;
+        }
+
+        for(int i = 0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            map[u]=v;
+        }
+        Arrays.fill(cnt, 0);
+        bfs();
+    }
+}
