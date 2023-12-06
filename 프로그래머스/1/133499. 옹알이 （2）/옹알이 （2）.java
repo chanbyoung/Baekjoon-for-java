0b118e6ffa45fe149e0b1c885795aac6c7c2a0c1
chanbyeong <122460524+chanbyoung@users.noreply.github.com>
2023-12-07 00:47:47 +0900

    [level 1] Title: 옹알이 (2), Time: 0.82 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -4,7 +4,7 @@ class Solution {
         for (int i = 0; i < babbling.length; i++) {
             if(babbling[i].contains("ayaaya") || babbling[i].contains("yeye") || babbling[i].contains("woowoo") || babbling[i].contains("mama"))
                 continue;
-            babbling[i] = babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("aya", " " );
             babbling[i] = babbling[i].replace("ye", " ");
             babbling[i] = babbling[i].replace("woo", " ");
             babbling[i] = babbling[i].replace("ma", " ");
