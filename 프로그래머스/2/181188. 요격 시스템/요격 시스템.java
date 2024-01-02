b694ecb268466b80c767f11e82e47adc3b86a52b
chanbyeong <122460524+chanbyoung@users.noreply.github.com>
2024-01-03 01:00:42 +0900

    [level 2] Title: 요격 시스템, Time: 1.54 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,24 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
-        int answer =1;
-        Arrays.sort(targets, (Comparator.comparingInt(x -> x[0])));
-        int preStart = targets[0][0];
-        int preEnd = targets[0][1];
+        int targetPoint = -1;
+        int answer =0;
+
+        Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));
 
         for (int[] target : targets) {
-            int curStart = target[0];
-            int curEnd = target[1];
-
-            if (preStart <= curStart && curStart < preEnd) {
-                preStart = Math.max(preStart, curStart);
-                preEnd = Math.min(preEnd, curEnd);
-            } else {
-                answer ++;
-                preStart = curStart;
-                preEnd = curEnd;
+            if (target[0] < targetPoint) {
+                continue;
             }
+            targetPoint = target[1];
+            answer ++;
+
         }
         return answer;
     }
+
 }
