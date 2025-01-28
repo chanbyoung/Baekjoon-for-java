e2c895fe4bc90c239939766c007ec82b78def451
chanbyeong <122460524+chanbyoung@users.noreply.github.com>
2025-01-28 23:46:58 +0900

    [Gold V] Title: 토마토, Time: 640 ms, Memory: 120296 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,80 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
-    static int map[][];
-    static int N, M, cnt;
-    static int[] cx = {1, 0, -1, 0};
-    static int[] cy = {0, 1, 0, -1};
-    static Queue<int[]> queue = new LinkedList<>();
+class Main {
 
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, 1, -1};
 
-    public static int bfs() {
-        while (!queue.isEmpty()) {
-            int x = queue.peek()[0];
-            int y = queue.peek()[1];
-            queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int X = x + cx[i];
-                int Y = y + cy[i];
-                if (X >= 0 && Y >= 0 && X < N && Y < M) {
-                    if (map[X][Y] == 0) {
-                        queue.add(new int[]{X, Y});
-                        map[X][Y] = map[x][y] + 1;
-                    }
-                }
-            }
+    private static String[][] box;
+    private static Queue<Tomato> q = new LinkedList<>();
+
+    static class Tomato {
+        int x;
+        int y;
+        int day;
+
+        public Tomato(int x, int y, int day) {
+            this.x = x;
+            this.y = y;
+            this.day = day;
         }
-        int max = Integer.MIN_VALUE;
-        if (zero()) {
-            return -1;
-        } else {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (max < map[i][j]) {
-                        max = map[i][j];
-                    }
-                }
-            }
-            return max - 1;
-        }
-    }
-
-
-
-
-
-
-    private static boolean zero() {
-        for(int i =0; i<N; i++){
-            for(int j=0;j<M;j++){
-                if(map[i][j]==0){
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        M = Integer.parseInt(st.nextToken()); //가로 크기
-        N = Integer.parseInt(st.nextToken()); //세로 크기
-        map = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < M; j++) {
-                int tomato = Integer.parseInt(st.nextToken());
-                map[i][j]= tomato;
-                if(tomato == 1){
-                    queue.add(new int[]{i,j});
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        box = new String[x][y];
+        for (int i = 0; i < x; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < y; j++) {
+                box[i][j] = st.nextToken();
+                if (box[i][j].equals("1")) {
+                    q.offer(new Tomato(i, j, 0));
                 }
             }
-        }
-        System.out.println(bfs());
-    }
-}
 
+        }
+        bfs();
+    }
+
+    private static void bfs() {
+        int day = 0;
+        while (!q.isEmpty()) {
+            Tomato tomato = q.poll();
+            day = tomato.day;
+            for (int i = 0; i < 4; i++) {
+                int cx = tomato.x + dx[i];
+                int cy = tomato.y + dy[i];
+
+                if (0 <= cx && cx < box.length && 0 <= cy && cy < box[0].length) {
+                    if (box[cx][cy].equals("0")) {
+                        box[cx][cy] = "1";
+                        q.offer(new Tomato(cx, cy, tomato.day+1));
+                    }
+                }
+            }
+
+        }
+        if (checkBox()) {
+            System.out.println(day);
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    private static boolean checkBox() {
+        for (String[] boxes : box) {
+            List<String> box = Arrays.asList(boxes);
+            if (box.contains("0")) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+}
